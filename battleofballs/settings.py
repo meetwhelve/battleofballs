8ca2553cbd2d6157d1a7854f41fd4d84c8d526f0
meetwhelve <2231618218@qq.com>
2023-01-22 15:37:55 +0800

    show the first page

--- battleofballs/settings.py
+++ battleofballs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!2)*3oz#=r22km^gs8pzggzgogv&@vp@xm-)asaxqbvo#qh64*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["139.196.8.108"]
 
 
 # Application definition
